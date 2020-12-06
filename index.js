59487a1888512ccaff3a6818f47edb9407a90c76
Kakan Ghosh <kakan.ghosh@welldev.io>
2020-12-06 16:44:49 +0600

    added memo

--- index.js
+++ index.js
@@ -19,70 +19,70 @@ const methodMappers = {
             return x.toString()
         },
         code: `a`,
-        result: {"test1":"1","test2":"3.141","test4":"-23"},
+        memo: {},
     },
     'Triple': {
         method: (x) => {
             return x * 3
         },
         code: `a`,
-        result: {"test1":6,"test2":12,"test4":-36,"test3":4.5,"test5":-4.5,"rnd_nrw_Adolphus":1680,"rnd_gnb_Meghan":1365,"rnd_xpa_Isaiah":2088},
+        memo: {},
     },
     'Float To int': {
         method: (x) => {
             return parseInt(x)
         },
         code: `a`,
-        result: {"test1":1,"test2":1,"test4":0,"test3":-1,"test5":52,"rnd_nrw_Adolphus":1680,"rnd_gnb_Meghan":1365,"rnd_xpa_Isaiah":2088},
+        memo: {},
     },
     'Is Odd': {
         method: (x) => {
             return x % 2 == 1
         },
         code: `a`,
-        result: {"test1":false,"test2":false,"test4":true,"test3":true,"test5":false,"rnd_nrw_Adolphus":1680,"rnd_gnb_Meghan":1365,"rnd_xpa_Isaiah":2088,"rnd_axr_Grover":false,"rnd_hna_Novella":false,"rnd_jgn_Polly":false},
+        memo: {},
     },
     'Square Root': {
         method: (x) => {
             return Math.sqrt(x)
         },
         code: `a`,
-        result: {"test1":2,"test2":4,"test4":12,"test3":1.5,"test5":10,"rnd_nrw_Adolphus":1680,"rnd_gnb_Meghan":1365,"rnd_xpa_Isaiah":2088,"rnd_axr_Grover":false,"rnd_hna_Novella":false,"rnd_jgn_Polly":false,"rnd_eqg_Wilbert":41095.3056,"rnd_gou_Genesis":28178.1654,"rnd_rpz_Dalton":4106.0736},
+        memo: {},
     },
     'Surface Area of a Cube': {
         method: (x) => {
             return parseFloat((6 * x *x).toFixed(4))
         },
         code: `a`,
-        result: {"test1":72600,"test2":5892486,"test4":807.36,"test3":2965.0374,"test5":22627.1286,"rnd_nrw_Adolphus":1680,"rnd_gnb_Meghan":1365,"rnd_xpa_Isaiah":2088,"rnd_axr_Grover":false,"rnd_hna_Novella":false,"rnd_jgn_Polly":false,"rnd_eqg_Wilbert":41095.3056,"rnd_gou_Genesis":28178.1654,"rnd_rpz_Dalton":4106.0736},
+        memo: {},
     },
     'String to Number': {
         method: (x) => {
             return parseFloat(x)
         },
         code: `a`,
-        result: {"test1":1,"test2":3.242342342342342e+23,"test4":87.45,"test3":-21,"test5":42,"rnd_nrw_Adolphus":1680,"rnd_gnb_Meghan":1365,"rnd_xpa_Isaiah":2088,"rnd_axr_Grover":false,"rnd_hna_Novella":false,"rnd_jgn_Polly":false,"rnd_eqg_Wilbert":41095.3056,"rnd_gou_Genesis":28178.1654,"rnd_rpz_Dalton":4106.0736,"rnd_odo_Misael":[],"rnd_exs_Robb":[808],"rnd_ygn_Elsa":[416,586]},
+        memo: {},
     },
     'Remove first 3 elements on array': {
         method: (x) => {
             return x.slice(3, x.length)
         },
         code: `.`,
-        result: {"test1":[89,876,738,211],"test2":[4,5],"test4":[963,844,809,8,431,620],"test3":[4],"test5":[],"rnd_nrw_Adolphus":1680,"rnd_gnb_Meghan":1365,"rnd_xpa_Isaiah":2088,"rnd_axr_Grover":false,"rnd_hna_Novella":false,"rnd_jgn_Polly":false,"rnd_eqg_Wilbert":41095.3056,"rnd_gou_Genesis":28178.1654,"rnd_rpz_Dalton":4106.0736,"rnd_odo_Misael":[],"rnd_exs_Robb":[808],"rnd_ygn_Elsa":[416,586]},
+        memo: {},
     },
     'Odd Elements': {
         method: (x) => {
             return x.filter((a, i) => i % 2 == 0)
         },
         code: `.`,
-        result: {"test1":["a","c","e","g","i","k"],"test2":["January","March"],"test4":"hiddenfile","test3":["Monday","Wednesday","Friday","Sunday"],"test5":"ext","rnd_nrw_Adolphus":1680,"rnd_gnb_Meghan":1365,"rnd_xpa_Isaiah":2088,"rnd_axr_Grover":false,"rnd_hna_Novella":false,"rnd_jgn_Polly":false,"rnd_eqg_Wilbert":41095.3056,"rnd_gou_Genesis":28178.1654,"rnd_rpz_Dalton":4106.0736,"rnd_odo_Misael":[],"rnd_exs_Robb":[808],"rnd_ygn_Elsa":[416,586],"test_1":3602717.0935,"test_2":303736.3531,"test_3":1317.0897,"test_4":185822.5414,"test_5":1057401.3101,"test_6":634264.2545,"rnd_tgj_Leonora":3490257.5851,"rnd_sfu_Florencio":5131.4481,"rnd_ybm_Mackenzie":95949.7018,"test6":"dots","test7":"many","test8":"with","test9":"mp3"},
+        memo: {},
     },
     'Double Index': {
         method: (x) => {
             return x.filter((a, i) => a == i * 2)
         },
         code: `.`,
-        result: {"test1":[0,2,4,6,8,10,12,14,16,18,20],"test2":[0,6],"test4":[],"test3":[2],"test5":[],"rnd_nrw_Adolphus":1680,"rnd_gnb_Meghan":1365,"rnd_xpa_Isaiah":2088,"rnd_axr_Grover":false,"rnd_hna_Novella":false,"rnd_jgn_Polly":false,"rnd_eqg_Wilbert":41095.3056,"rnd_gou_Genesis":28178.1654,"rnd_rpz_Dalton":4106.0736,"rnd_odo_Misael":[],"rnd_exs_Robb":[808],"rnd_ygn_Elsa":[416,586],"test_1":3602717.0935,"test_2":303736.3531,"test_3":1317.0897,"test_4":185822.5414,"test_5":1057401.3101,"test_6":634264.2545,"rnd_tgj_Leonora":3490257.5851,"rnd_sfu_Florencio":5131.4481,"rnd_ybm_Mackenzie":95949.7018,"test6":"dots","test7":"many","test8":"with","test9":"mp3","rnd_pqm_Kamille":[0,4,6,16,18,20],"rnd_jyh_Aglae":[2,4,8,10,14,20,22,26],"rnd_kmg_Maximus":[2,6,8,10,14,16,18,24,28,30,32]},
+        memo: {},
     },
     'Only Vowels': {
         method: (x) => {
@@ -97,7 +97,7 @@ const methodMappers = {
             return true;
         },
         code: `.`,
-        result: {"test1":false,"test2":true,"test4":true,"test3":true,"test5":false,"rnd_nrw_Adolphus":1680,"rnd_gnb_Meghan":1365,"rnd_xpa_Isaiah":2088,"rnd_axr_Grover":false,"rnd_hna_Novella":false,"rnd_jgn_Polly":false,"rnd_eqg_Wilbert":41095.3056,"rnd_gou_Genesis":28178.1654,"rnd_rpz_Dalton":4106.0736,"rnd_odo_Misael":[],"rnd_exs_Robb":[808],"rnd_ygn_Elsa":[416,586],"test_1":3602717.0935,"test_2":303736.3531,"test_3":1317.0897,"test_4":185822.5414,"test_5":1057401.3101,"test_6":634264.2545,"rnd_tgj_Leonora":3490257.5851,"rnd_sfu_Florencio":5131.4481,"rnd_ybm_Mackenzie":95949.7018,"test6":true,"test7":false,"test8":"with","test9":"mp3","rnd_pqm_Kamille":[0,4,6,16,18,20],"rnd_jyh_Aglae":[2,4,8,10,14,20,22,26],"rnd_kmg_Maximus":[2,6,8,10,14,16,18,24,28,30,32],"flat":[0,1,2],"empty":[],"nested1":[0,1],"nested2":[0,1,2,3,4,5]},
+        memo: {},
     },
     'Flatten2': {
         method: (x) => {
@@ -115,14 +115,14 @@ const methodMappers = {
             return res;
         },
         code: `.`,
-        result: {"test1":[0,2,4,6,8,10,12,14,16,18,20],"test2":[0,6],"test4":[],"test3":[2],"test5":[],"rnd_nrw_Adolphus":1680,"rnd_gnb_Meghan":1365,"rnd_xpa_Isaiah":2088,"rnd_axr_Grover":false,"rnd_hna_Novella":false,"rnd_jgn_Polly":false,"rnd_eqg_Wilbert":41095.3056,"rnd_gou_Genesis":28178.1654,"rnd_rpz_Dalton":4106.0736,"rnd_odo_Misael":[],"rnd_exs_Robb":[808],"rnd_ygn_Elsa":[416,586],"test_1":3602717.0935,"test_2":303736.3531,"test_3":1317.0897,"test_4":185822.5414,"test_5":1057401.3101,"test_6":634264.2545,"rnd_tgj_Leonora":3490257.5851,"rnd_sfu_Florencio":5131.4481,"rnd_ybm_Mackenzie":95949.7018,"test6":"dots","test7":"many","test8":"with","test9":"mp3","rnd_pqm_Kamille":[0,4,6,16,18,20],"rnd_jyh_Aglae":[2,4,8,10,14,20,22,26],"rnd_kmg_Maximus":[2,6,8,10,14,16,18,24,28,30,32],"flat":[0,1,2],"empty":[],"nested1":[0,1],"nested2":[0,1,2,3,4,5]},
+        memo: {},
     },
     'SphereVolume': {
         method: (x) => {
             return parseFloat(((4/3) * Math.PI * x * x * x).toFixed(4))
         },
         code: `.`,
-        result: {"test1":1,"test2":3.242342342342342e+23,"test4":87.45,"test3":-21,"test5":42,"rnd_nrw_Adolphus":1680,"rnd_gnb_Meghan":1365,"rnd_xpa_Isaiah":2088,"rnd_axr_Grover":false,"rnd_hna_Novella":false,"rnd_jgn_Polly":false,"rnd_eqg_Wilbert":41095.3056,"rnd_gou_Genesis":28178.1654,"rnd_rpz_Dalton":4106.0736,"rnd_odo_Misael":[],"rnd_exs_Robb":[808],"rnd_ygn_Elsa":[416,586],"test_1":3602717.0935,"test_2":303736.3531,"test_3":1317.0897,"test_4":185822.5414,"test_5":1057401.3101,"test_6":634264.2545,"rnd_tgj_Leonora":3490257.5851,"rnd_sfu_Florencio":5131.4481,"rnd_ybm_Mackenzie":95949.7018},
+        memo: {},
     },
     'Get File Extension': {
         method: (x) => {
@@ -130,7 +130,7 @@ const methodMappers = {
             return sp.length > 1 ? sp[sp.length - 1] : ''
         },
         code: `.`,
-        result: {"test1":"","test2":"","test4":"hiddenfile","test3":"txt","test5":"ext","rnd_nrw_Adolphus":1680,"rnd_gnb_Meghan":1365,"rnd_xpa_Isaiah":2088,"rnd_axr_Grover":false,"rnd_hna_Novella":false,"rnd_jgn_Polly":false,"rnd_eqg_Wilbert":41095.3056,"rnd_gou_Genesis":28178.1654,"rnd_rpz_Dalton":4106.0736,"rnd_odo_Misael":[],"rnd_exs_Robb":[808],"rnd_ygn_Elsa":[416,586],"test_1":3602717.0935,"test_2":303736.3531,"test_3":1317.0897,"test_4":185822.5414,"test_5":1057401.3101,"test_6":634264.2545,"rnd_tgj_Leonora":3490257.5851,"rnd_sfu_Florencio":5131.4481,"rnd_ybm_Mackenzie":95949.7018,"test6":"dots","test7":"many","test8":"with","test9":"mp3"},
+        memo: {},
     },
     'Invert Case': {
         method: (x) => {
@@ -147,7 +147,7 @@ const methodMappers = {
             return res;
         },
         code: `.`,
-        result: {"test1":false,"test2":true,"test4":true,"test3":true,"test5":false,"rnd_nrw_Adolphus":1680,"rnd_gnb_Meghan":1365,"rnd_xpa_Isaiah":2088,"rnd_axr_Grover":false,"rnd_hna_Novella":false,"rnd_jgn_Polly":false,"rnd_eqg_Wilbert":41095.3056,"rnd_gou_Genesis":28178.1654,"rnd_rpz_Dalton":4106.0736,"rnd_odo_Misael":[],"rnd_exs_Robb":[808],"rnd_ygn_Elsa":[416,586],"test_1":3602717.0935,"test_2":303736.3531,"test_3":1317.0897,"test_4":185822.5414,"test_5":1057401.3101,"test_6":634264.2545,"rnd_tgj_Leonora":3490257.5851,"rnd_sfu_Florencio":5131.4481,"rnd_ybm_Mackenzie":95949.7018,"test6":true,"test7":false,"test8":"with","test9":"mp3","rnd_pqm_Kamille":[0,4,6,16,18,20],"rnd_jyh_Aglae":[2,4,8,10,14,20,22,26],"rnd_kmg_Maximus":[2,6,8,10,14,16,18,24,28,30,32],"flat":[0,1,2],"empty":"","nested1":[0,1],"nested2":[0,1,2,3,4,5],"mixed":"fLiRpYdUCK","all_lowercase":"FUNKEY MONKEY","all_uppercase":"monkey magic","rnd_pku_Will":"VOLUPTAS RATIONE","rnd_jnd_Cynthia":"EIUS 6I EXCEPTURI ullam SOLUTA VOLUPTAS neque EOS AUT cumque","rnd_ewx_Robb":"SIT"},
+        memo: {},
     },
     'Sorting Type': {
         method: (x) => {
@@ -162,7 +162,7 @@ const methodMappers = {
             else if (descending) return -1;
         },
         code: `.`,
-        result: {"test1":1,"test2":0,"test4":-1,"test3":0,"test5":1,"rnd_nrw_Adolphus":1680,"rnd_gnb_Meghan":1365,"rnd_xpa_Isaiah":2088,"rnd_axr_Grover":false,"rnd_hna_Novella":false,"rnd_jgn_Polly":false,"rnd_eqg_Wilbert":41095.3056,"rnd_gou_Genesis":28178.1654,"rnd_rpz_Dalton":4106.0736,"rnd_odo_Misael":[],"rnd_exs_Robb":[808],"rnd_ygn_Elsa":[416,586],"test_1":3602717.0935,"test_2":303736.3531,"test_3":1317.0897,"test_4":185822.5414,"test_5":1057401.3101,"test_6":634264.2545,"rnd_tgj_Leonora":3490257.5851,"rnd_sfu_Florencio":5131.4481,"rnd_ybm_Mackenzie":95949.7018,"test6":1,"test7":-1,"test8":0,"test9":1,"rnd_pqm_Kamille":[0,4,6,16,18,20],"rnd_jyh_Aglae":[2,4,8,10,14,20,22,26],"rnd_kmg_Maximus":[2,6,8,10,14,16,18,24,28,30,32],"flat":[0,1,2],"empty":"","nested1":[0,1],"nested2":[0,1,2,3,4,5],"mixed":"fLiRpYdUCK","all_lowercase":"FUNKEY MONKEY","all_uppercase":"monkey magic","rnd_pku_Will":"VOLUPTAS RATIONE","rnd_jnd_Cynthia":"EIUS 6I EXCEPTURI ullam SOLUTA VOLUPTAS neque EOS AUT cumque","rnd_ewx_Robb":"SIT","test10":-1,"rnd_pas_Brock":-1,"rnd_jqe_Liam":-1,"rnd_owc_Perry":1},
+        memo: {},
     },
     'Most Frequent': {
         method: (x) => {
@@ -184,7 +184,7 @@ const methodMappers = {
             return freqDist.key;
         },
         code: `.`,
-        result: {"test1":1,"test2":0,"test4":-1,"test3":0,"test5":1,"rnd_nrw_Adolphus":1680,"rnd_gnb_Meghan":1365,"rnd_xpa_Isaiah":2088,"rnd_axr_Grover":false,"rnd_hna_Novella":false,"rnd_jgn_Polly":false,"rnd_eqg_Wilbert":41095.3056,"rnd_gou_Genesis":28178.1654,"rnd_rpz_Dalton":4106.0736,"rnd_odo_Misael":[],"rnd_exs_Robb":[808],"rnd_ygn_Elsa":[416,586],"test_1":3602717.0935,"test_2":303736.3531,"test_3":1317.0897,"test_4":185822.5414,"test_5":1057401.3101,"test_6":634264.2545,"rnd_tgj_Leonora":3490257.5851,"rnd_sfu_Florencio":5131.4481,"rnd_ybm_Mackenzie":95949.7018,"test6":1,"test7":-1,"test8":0,"test9":1,"rnd_pqm_Kamille":[0,4,6,16,18,20],"rnd_jyh_Aglae":[2,4,8,10,14,20,22,26],"rnd_kmg_Maximus":[2,6,8,10,14,16,18,24,28,30,32],"flat":[0,1,2],"empty":"","nested1":[0,1],"nested2":[0,1,2,3,4,5],"mixed":"fLiRpYdUCK","all_lowercase":"FUNKEY MONKEY","all_uppercase":"monkey magic","rnd_pku_Will":"VOLUPTAS RATIONE","rnd_jnd_Cynthia":"EIUS 6I EXCEPTURI ullam SOLUTA VOLUPTAS neque EOS AUT cumque","rnd_ewx_Robb":"SIT","test10":-1,"rnd_pas_Brock":-1,"rnd_jqe_Liam":-1,"rnd_owc_Perry":1,"few":1,"mix":5,"mix2":3,"zero":0,"rnd_ywv_Joshuah":862,"rnd_syr_Axel":494,"rnd_ocp_Colten":362},
+        memo: {},
     },
     'Has Balance Points': {
         method: (x) => {
@@ -211,7 +211,7 @@ const methodMappers = {
               return false;
         },
         code: `.`,
-          result: {"test1":1,"test2":0,"test4":-1,"test3":0,"test5":1,"rnd_nrw_Adolphus":1680,"rnd_gnb_Meghan":1365,"rnd_xpa_Isaiah":2088,"rnd_axr_Grover":false,"rnd_hna_Novella":false,"rnd_jgn_Polly":false,"rnd_eqg_Wilbert":41095.3056,"rnd_gou_Genesis":28178.1654,"rnd_rpz_Dalton":4106.0736,"rnd_odo_Misael":[],"rnd_exs_Robb":[808],"rnd_ygn_Elsa":[416,586],"test_1":3602717.0935,"test_2":303736.3531,"test_3":1317.0897,"test_4":185822.5414,"test_5":1057401.3101,"test_6":634264.2545,"rnd_tgj_Leonora":3490257.5851,"rnd_sfu_Florencio":5131.4481,"rnd_ybm_Mackenzie":95949.7018,"test6":1,"test7":-1,"test8":0,"test9":1,"rnd_pqm_Kamille":[0,4,6,16,18,20],"rnd_jyh_Aglae":[2,4,8,10,14,20,22,26],"rnd_kmg_Maximus":[2,6,8,10,14,16,18,24,28,30,32],"flat":[0,1,2],"empty":true,"nested1":[0,1],"nested2":[0,1,2,3,4,5],"mixed":"fLiRpYdUCK","all_lowercase":"FUNKEY MONKEY","all_uppercase":"monkey magic","rnd_pku_Will":"VOLUPTAS RATIONE","rnd_jnd_Cynthia":"EIUS 6I EXCEPTURI ullam SOLUTA VOLUPTAS neque EOS AUT cumque","rnd_ewx_Robb":"SIT","test10":-1,"rnd_pas_Brock":-1,"rnd_jqe_Liam":-1,"rnd_owc_Perry":1,"few":1,"mix":5,"mix2":3,"zero":true,"rnd_ywv_Joshuah":862,"rnd_syr_Axel":494,"rnd_ocp_Colten":362,"bad1":false,"good1":true,"rnd_abj_Odessa":false,"rnd_lgt_Marianna":true,"rnd_lbx_Orlo":false},
+        memo: {},
     },
     'Return the Rank of a Day in a Year': {
         method: (x) => {
@@ -221,6 +221,7 @@ const methodMappers = {
             return diff / (1000 * 60 * 60 * 24)
         },
         code: '.',
+        memo: {},
     },
 }
 
@@ -250,7 +251,16 @@ async function attemptTask(arguments) {
                         arguments.testsJson[property] = tests_json[property].result;
                     } else {
                         //calculating.push('Calculating for ' + data.nextTask.title);
-                        arguments.testsJson[property] = methodMapper.method(tests_json[property].args[0]);
+                        let argument = tests_json[property].args[0];
+                        if (!!methodMapper.memo[argument]) {
+                            //console.log('using memo :D ', argument, methodMapper.memo[argument]);
+                            arguments.testsJson[property] = methodMapper.memo[argument];
+                        } else {
+                            let result = methodMapper.method(argument);
+                            arguments.testsJson[property] = result;
+                            methodMapper.memo[argument] = result;
+                        }
+                        
                     }
                 }
                 arguments.code = methodMapper.code;
