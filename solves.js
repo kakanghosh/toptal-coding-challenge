8f14be65fbf957ebe29e6f6da5c7701563579217
Kakan Ghosh <kakan.ghosh@welldev.io>
2020-12-06 22:19:34 +0600

    moduler solution

---
+++ solves.js
@@ -0,0 +1,213 @@
+const methodMappers = {
+    'Number To String': {
+        method: (x) => {
+            return x.toString()
+        },
+        code: `.`,
+        memo: {},
+    },
+    'Triple': {
+        method: (x) => {
+            return x * 3
+        },
+        code: `.`,
+        memo: {},
+    },
+    'Float To int': {
+        method: (x) => {
+            return parseInt(x)
+        },
+        code: `.`,
+        memo: {},
+    },
+    'Is Odd': {
+        method: (x) => {
+            return x % 2 == 1
+        },
+        code: `.`,
+        memo: {},
+    },
+    'Square Root': {
+        method: (x) => {
+            return Math.sqrt(x)
+        },
+        code: `.`,
+        memo: {},
+    },
+    'Surface Area of a Cube': {
+        method: (x) => {
+            return parseFloat((6 * x *x).toFixed(4))
+        },
+        code: `.`,
+        memo: {},
+    },
+    'String to Number': {
+        method: (x) => {
+            return parseFloat(x)
+        },
+        code: `.`,
+        memo: {},
+    },
+    'Remove first 3 elements on array': {
+        method: (x) => {
+            return x.slice(3, x.length)
+        },
+        code: `.`,
+        memo: {},
+    },
+    'Odd Elements': {
+        method: (x) => {
+            return x.filter((a, i) => i % 2 == 0)
+        },
+        code: `.`,
+        memo: {},
+    },
+    'Double Index': {
+        method: (x) => {
+            return x.filter((a, i) => a == i * 2)
+        },
+        code: `.`,
+        memo: {},
+    },
+    'Only Vowels': {
+        method: (x) => {
+            for(let c in x) {
+                let upperChar = x[c].toUpperCase();
+                if (upperChar === 'A' ||  upperChar === 'E' || upperChar === 'I' || upperChar === 'O' || upperChar === 'U') {
+                    continue;
+                } else {
+                    return false;
+                }
+            }
+            return true;
+        },
+        code: `.`,
+        memo: {},
+    },
+    'Flatten2': {
+        method: (x) => {
+            let res = []
+            function cal(a) {
+              a.forEach(n => {
+                if (Array.isArray(n)) {
+                    cal(n);
+                } else {
+                    res.push(n);
+                }
+              })
+            }
+            cal(x);
+            return res;
+        },
+        code: `.`,
+        memo: {},
+    },
+    'SphereVolume': {
+        method: (x) => {
+            return parseFloat(((4/3) * Math.PI * x * x * x).toFixed(4))
+        },
+        code: `.`,
+        memo: {},
+    },
+    'Get File Extension': {
+        method: (x) => {
+            const sp = x.split('.')
+            return sp.length > 1 ? sp[sp.length - 1] : ''
+        },
+        code: `.`,
+        memo: {},
+    },
+    'Invert Case': {
+        method: (x) => {
+            let res = '';
+            for(let c in x) {
+                if (x.charCodeAt(c) >= 65 && x.charCodeAt(c) <= 90) {
+                    res += x[c].toLowerCase();
+                } else if (x.charCodeAt(c) >= 97 && x.charCodeAt(c) <= 122) {
+                    res += x[c].toUpperCase();
+                } else {
+                    res += x[c];
+                }
+            }
+            return res;
+        },
+        code: `.`,
+        memo: {},
+    },
+    'Sorting Type': {
+        method: (x) => {
+            let ascending = x[0] < x[1];
+            let descending = !ascending;
+            for(let i = 1; i < x.length; i++) {
+                if ( (ascending && x[i] < x[i-1]) || (descending && x[i] > x[i-1]) ) {
+                    return 0;
+                }
+            }
+            if (ascending) return 1;
+            else if (descending) return -1;
+        },
+        code: `.`,
+        memo: {},
+    },
+    'Most Frequent': {
+        method: (x) => {
+            let freq = {}
+            let freqDist;
+
+            x.forEach(key => {
+                if (!!freq[key]) {
+                    freq[key] += 1
+                } else {
+                    freq[key] = 1
+                }
+                if (!!freqDist && freqDist.value < freq[key]) {
+                    freqDist = {key: parseInt(key), value: freq[key]}
+                } else if (!(!!freqDist)) {
+                    freqDist = {key: parseInt(key), value: freq[key]}
+                }
+            })
+            return freqDist.key;
+        },
+        code: `.`,
+        memo: {},
+    },
+    'Has Balance Points': {
+        method: (x) => {
+            if (x.length > 0) {
+                for(let i in x) {
+                    let sumBefore = 0;
+                    for (let j = 0; j < i; j++) {
+                        sumBefore += x[j];
+                    }
+                    let sumAfter = 0;
+                    for (let j = i; j < x.length; j++) {
+                        sumAfter += x[j];
+                        if (sumAfter > sumBefore) {
+                            break;
+                        }
+                    }
+                  if (sumBefore === sumAfter) {
+                    return true;
+                  }
+                }
+              } else{
+                return true;
+              }
+              return false;
+        },
+        code: `.`,
+        memo: {},
+    },
+    'Return the Rank of a Day in a Year': {
+        method: (x) => {
+            let initDate = new Date('12/31/2018')
+            let date = new Date(x)
+            const diff = date - initDate;
+            return diff / (1000 * 60 * 60 * 24)
+        },
+        code: '.',
+        memo: {},
+    },
+}
+
+module.exports = methodMappers;
