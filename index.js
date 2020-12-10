7d72cabaf007c93969c6c68024d463bd05122004
Kakan Ghosh <kakan.ghosh@welldev.io>
2020-12-10 23:58:39 +0600

    added caching

--- index.js
+++ index.js
@@ -39,7 +39,7 @@ async function attemptTask(args) {
         const form = new FormData();
         form.append('attempt_id', args.attemptId);
         form.append('entry_key', ENTRY_KEY);
-        form.append('tests_json', JSON.stringify(Object.fromEntries(Object.entries(tests_json).map( ([key, value]) => [key, value.result || methodMapper.method(value.args[0])] ))));
+        form.append('tests_json', JSON.stringify(Object.fromEntries(Object.entries(tests_json).map( ([key, value]) => [key, value.result || memo[value.args[0]] || cachingResult(methodMapper, value.args[0])] ))));
         form.append('code', methodMapper.code);
         const response = await axios.post(`${BASE_URL}/webappApi/entry/${ENTRY_ID}/attemptTask`, form, {
             headers: {
