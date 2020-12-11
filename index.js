5b7b075a8b8cbe9a34ea0026070321ee3594e23b
Kakan Ghosh <kakan.ghosh@welldev.io>
2020-12-11 11:56:13 +0600

    increase max attempt

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ const headers = {
     accept: 'application/json, text/javascript, */*; q=0.01', 
 }
 let attempCount = 0;
-let MAX_ATTEMP = 25 * 0;
+let MAX_ATTEMP = 25 * 4;
 const totalPoints = [];
 
 let cachingResult = (methodMapper, arg) => {
